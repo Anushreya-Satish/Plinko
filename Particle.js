a084d29ab5778758ffc915979be01e9de6fbbf81
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2021-08-03 20:49:49 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -4,7 +4,7 @@ class Particle {
         restitution: 0.4,
         isStatic: false
         }
-        this.r = 8;
+        this.r = r;
         this.body = Bodies.circle(x, y, this.r, options);
         this.color = color(random(0, 255), random(0,255), random(0, 255));
         World.add(world, this.body);
